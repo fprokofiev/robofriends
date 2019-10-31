84000261fb1e8eae26bd466f3c3a4570f2a5f70d
Fyodor Prokofiev <9195737+fprokofiev@users.noreply.github.com>
2019-10-31 17:14:14 +0300

    Updates

---
+++ precache-manifest.8bde654fc871137d13013094f2129696.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e43afb1a283e948c6763d22152ba493e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "57db8ef0007bed3833f2",
+    "url": "/robofriends/static/css/main.2bc858dd.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "57db8ef0007bed3833f2",
+    "url": "/robofriends/static/js/main.40eacf38.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
